fcd1550f94f7a8b008bd1b29c1fb6ae4e720100a
Juli4nHuber <juliangabrielhr@gmail.com>
2023-09-06 10:26:15 -0300

    Ejercicio1 terminado

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,27 +1,47 @@
 import './app.css';
 import {SpotifyListRow} from './SpotifyListRow.jsx';
 
+let songs = [
+  {
+    imgUrl: 'https://i.scdn.co/image/ab67616d0000b273da5d5aeeabacacc1263c0f4b',
+    nameSong: 'Dress',
+    artistName: 'Taylor Swift',
+    songTime: 3.50,
+    isLike: true
+  },
+  {
+    imgUrl: 'https://i.scdn.co/image/ab67616d0000b27308ccf011ad52680964601764',
+    nameSong: 'Lipstick',
+    artistName: 'Charlie Puth',
+    songTime: 3.28,
+    isLike: false
+  },
+  {
+    imgUrl: 'https://i.scdn.co/image/ab67616d0000b273700f7bf79c9f063ad0362bdf',
+    nameSong: 'Bet U Wanna',
+    artistName: 'Sabrina Carpenter',
+    songTime: 3.11,
+    isLike: true
+  }
+]
+
 export function App () {
   return (
     <>
-      <SpotifyListRow
-        imgUrl='https://i.scdn.co/image/ab67616d0000b273da5d5aeeabacacc1263c0f4b'
-        nameSong='Dress'
-        artistName='Taylor Swift'
-        songTime='3.14'
-      />
-      <SpotifyListRow
-        imgUrl='https://i.scdn.co/image/ab67616d0000b27308ccf011ad52680964601764'
-        nameSong='Lipstick'
-        artistName='Charlie Puth'
-        songTime='3.04'
-      />
-      <SpotifyListRow
-        imgUrl='https://i.scdn.co/image/ab67616d0000b273700f7bf79c9f063ad0362bdf'
-        nameSong='Bet U Wanna'
-        artistName='Sabrina Carpenter'
-        songTime='3.12'
-      />
+      {
+        songs.map(({imgUrl, nameSong, artistName, songTime, isLike}, index) => {
+          return(
+            <SpotifyListRow
+              key = {index}
+              imgUrl = {imgUrl}
+              nameSong = {nameSong}
+              artistName = {artistName}
+              songTime = {songTime.toFixed(2)}
+              initialIsLike = {isLike}
+            />
+          );
+        })
+      }
     </>
   )
 }
--- projects/00-hola-mundo/src/SpotifyListRow.jsx
+++ projects/00-hola-mundo/src/SpotifyListRow.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import ReactPropTypesfrom from "react";
-
-export function SpotifyListRow({ imgUrl, nameSong, artistName, songTime}){
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as faHeartSolid} from '@fortawesome/free-solid-svg-icons' ;
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+export function SpotifyListRow({ imgUrl, nameSong, artistName, songTime, initialIsLike }){
+    const [isLike, setIsLike] = useState(initialIsLike);
     return (
         <div className="sp-list-row">
             <div className="sp-list-row-song">
-            <img className="sp-list-row-img" src={imgUrl} alt="Album de cover"/>
-            <div className="sp-list-row-data">
-                <p className="sp-list-row-name">{nameSong}</p>
-                <p className="sp-list-row-artist">{artistName}</p>
+                <img className="sp-list-row-img" src={imgUrl} alt="Album de cover"/>
+                <div className="sp-list-row-data">
+                    <p className="sp-list-row-name">{nameSong}</p>
+                    <p className="sp-list-row-artist">{artistName}</p>
+                </div>
             </div>
+            <div className="sp-list-row-like-container">
+                <FontAwesomeIcon className="sp-list-row-like" icon={ isLike ? faHeartSolid : faHeartRegular} onClick={() => setIsLike(!isLike)}/>
             </div>
             <p className="sp-list-row-time">{songTime}</p>
         </div>
